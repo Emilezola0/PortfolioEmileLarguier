4e0a695b25f97b3ed7cddea58fe72c636b3c3f2c
Emilezola0 <emilelarguier2@gmail.com>
2025-04-21 18:34:11 +0200

    Update main

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ import { Mob } from "./Mob.js";
 import { Bullet } from "./Bullet.js";
 import { spawnManager } from "./spawnManager.js";
 import { Particle } from "./Particle.js";
-import { voidParticle } from "./VoidParticle.js";
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
@@ -67,7 +66,7 @@ function updateGame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     voidZone.draw(ctx);
 
-    // GÃ©nÃ©rer des void particles en orbite
+    // Générer des void particles en orbite
     if (Math.random() < 0.3) {
         const angle = Math.random() * Math.PI * 2;
         const radius = voidZone.radius + 30 + Math.random() * 40;
@@ -76,7 +75,7 @@ function updateGame() {
         voidParticles.push(new Particle(x, y, "purple", angle, radius));
     }
 
-    // Met Ã  jour et dessine les folders (update d'abord, draw ensuite pour le z-index)
+    // Met à jour et dessine les folders (update d'abord, draw ensuite pour le z-index)
     for (const folder of folders) {
         folder.update(mobs, bullets, voidZone.center, voidZone.radius);
     }
@@ -103,7 +102,7 @@ function updateGame() {
         p.draw(ctx);
     }
 
-    // Supprimer les folders aspirÃ©s visuellement
+    // Supprimer les folders aspirés visuellement
     for (let i = folders.length - 1; i >= 0; i--) {
         const f = folders[i];
         if (f.absorbing && f.opacity <= 0) {
