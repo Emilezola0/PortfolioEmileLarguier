59665bcf12fd2e4b16cd7a7b50e6e15b0ef27bea
Emilezola0 <emilelarguier2@gmail.com>
2025-04-24 14:02:20 +0200

    fix name in project_2022

--- js/projects/project_2022.js
+++ js/projects/project_2022.js
@@ -3,6 +3,6 @@ export function getProjectContent() {
     return `
         <h2>2022 - Mon portfolio</h2>
         <p>Gameplay exemple for my game scryptalking, made at LISAA !</p>
-        <img src="assets/projects/2021/gameplay1.png" style="max-width: 100%;">
+        <img src="assets/projects/2022/gameplay1.png" style="max-width: 100%;">
     `;
 }
