1d4cb47ada17ec01d902c8d550fb501bdaf2197e
Emilezola0 <emilelarguier2@gmail.com>
2025-04-27 14:53:33 +0200

    Update debris values

--- js/debrisTypes.js
+++ js/debrisTypes.js
@@ -1,62 +1,62 @@
 export const debrisTypes = [
     {
         name: "basic",
-        hp: 2,
+        hp: 3,
         image: "mine.png",
         nutrition: 5,
         scale: 1,
         scrapNumber: 2,
-        speed: 0.5,
+        speed: 0.8,
         waveMin: 1,
-        waveMax: -1
+        waveMax: 10
     },
     {
         name: "bigScrap",
-        hp: 3,
+        hp: 6,
         image: "asteroid.png",
         nutrition: 15,
         scale: 1.8,
         scrapNumber: 5,
-        speed: 0.3,
+        speed: 0.5,
         waveMin: 2,
-        waveMax: -1
+        waveMax: 15
     },
     {
         name: "tiny",
-        hp: 1,
+        hp: 2,
         image: "rocket.png",
         nutrition: 2,
-        scale: 0.8,
+        scale: 1,
         scrapNumber: 1,
-        speed: 0.8,
+        speed: 1.2,
         waveMin: 1,
-        waveMax: -1
+        waveMax: 10
     },
     {
         name: "bigAsteroid",
-        hp: 15,
+        hp: 25,
         image: "bigAsteroid.png",
         nutrition: 10,
         scale: 2,
         scrapNumber: 20,
-        speed: 0.3,
+        speed: 0.4,
         waveMin: 5,
-        waveMax: -1
+        waveMax: 25
     },
     {
         name: "basicFast",
-        hp: 4,
+        hp: 6,
         image: "rocket.png",
-        nutrition: 1,
-        scale: 0.25,
-        scrapNumber: 1,
-        speed: 1.25,
+        nutrition: 2,
+        scale: 0.8,
+        scrapNumber: 3,
+        speed: 1.5,
         waveMin: 5,
         waveMax: -1
     },
     {
         name: "UFO",
-        hp: 15,
+        hp: 40,
         image: "ufo.png",
         nutrition: 4,
         scale: 1,
@@ -70,9 +70,9 @@ export const debrisTypes = [
         hp: 50,
         image: "object1.png",
         nutrition: 25,
-        scale: 4,
+        scale: 3,
         scrapNumber: 20,
-        speed: 0.25,
+        speed: 0.3,
         waveMin: 10,
         waveMax: -1
     },
@@ -81,10 +81,10 @@ export const debrisTypes = [
         hp: 50,
         image: "object2.png",
         nutrition: 25,
-        scale: 4,
-        scrapNumber: 20,
-        speed: 0.5,
-        waveMin: 10,
+        scale: 3,
+        scrapNumber: 30,
+        speed: 0.6,
+        waveMin: 15,
         waveMax: -1
     },
     {
@@ -92,10 +92,21 @@ export const debrisTypes = [
         hp: 100,
         image: "object3.png",
         nutrition: 25,
-        scale: 4,
-        scrapNumber: 20,
-        speed: 0.2,
-        waveMin: 10,
+        scale: 3,
+        scrapNumber: 30,
+        speed: 0.3,
+        waveMin: 15,
+        waveMax: -1
+    },
+    {
+        name: "BigPlanet",
+        hp: 250,
+        image: "object1.png",
+        nutrition: 25,
+        scale: 5,
+        scrapNumber: 100,
+        speed: 0.3,
+        waveMin: 15,
         waveMax: -1
     }
 ];
